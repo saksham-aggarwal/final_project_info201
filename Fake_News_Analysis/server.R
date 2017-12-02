dc1ccbccdaf1d18e5a2825b46924b58d0fd2778b
Hayden Hong <afruitpie@gmail.com>
2017-12-02 12:10:39 -0800

    Added initial search function

--- Fake_News_Analysis/server.R
+++ Fake_News_Analysis/server.R
@@ -2,10 +2,19 @@ library(shiny)
 library(dplyr)
 library(stringr)
 library(data.table)
+library(ggplot2)
 
 # Data Frame for Fake News
 fakeNewsDataFrame <- fread("../data/fake.csv")
 
 shinyServer(function(input, output) {
   # Insert server functions here
+  searchCount <- reactive({
+    # TODO: Make this output a ggplot pie chart of found articles with search query compared to number of all articles 
+    searchNewsCount <- nrow(dplyr::filter(fakeNewsDataFrame, str_detect(fakeNewsDataFrame$text, input$search)))
+  })
+  
+  
+  output$countFound <- renderText(searchCount())
+  
 })
--- Fake_News_Analysis/ui.R
+++ Fake_News_Analysis/ui.R
@@ -3,18 +3,26 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(
   fluidPage(
+    
   # Application title
     titlePanel("Getting Real About Fake News"),
 
+    #FIXME: put the textInput in the correct place
+    textInput("search", "Search"),
+
     # Sidebar for user input
     sidebarLayout(
-      
       sidebarPanel(
-        # Insert sidebar input her
+        
+        
       ),
       
       mainPanel(
         # Insert plots and data here
+        
+        # TODO: Make this output ggplot, see server file for more details
+        textOutput("countFound")
+        
       )
     )
   )
