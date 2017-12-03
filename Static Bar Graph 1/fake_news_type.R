c1aa353f48f066b3fed59b2008f8239cd7ccd0e5
Mrinoy Banerjee <mrinoybanerjee@D-128-95-66-107.dhcp4.washington.edu>
2017-12-02 17:42:52 -0800

    mcommit1

---
+++ Static Bar Graph 1/fake_news_type.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(ggplot2)
+
+data_set <- read.csv("~/info 201/final_project_info201/data/fake.csv", stringsAsFactors = FALSE)
+
+bar.Graph <- function(dataset) {
+  
+#bs <- nrow(filter(dataset, type == "bs"))
+conspiracy <- nrow(filter(dataset, type == "conspiracy"))
+fake <- nrow(filter(dataset, type == "fake"))
+satire <- nrow(filter(dataset, type == "satire"))
+hate <- nrow(filter(dataset, type == "hate"))
+junksci <- nrow(filter(dataset, type == "junksci"))
+state <- nrow(filter(dataset, type == "state"))
+
+data <- data.frame( 
+  Types <- c("Conspiracy", "Fake", "Satire", "Hate", "Junksci", "State"),
+  Categories <- c(conspiracy, fake, satire, hate, junksci, state)
+)
+
+bar_graph <- ggplot(data, aes(x = Types , y = Categories, fill = Types)) +
+  geom_bar(stat="identity")
+
+  return(bar_graph)
+}
+
+graph <- bar.Graph(data_set)
+ggsave(filename = "fake_news_type.jpg", plot = graph)
